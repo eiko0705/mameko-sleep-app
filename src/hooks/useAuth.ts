b2ee684ce3482bbbdb7c285a3e1568cdd58c1d41
eiko0705 <eiko.imai.070502@gmail.com>
2024-08-12 16:14:10 +0900

    feat: create auth state watch function

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,3 +1,18 @@
 import supabase from '../lib/supabase';
 import { Session } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
+
+const useAuth = () => {
+  const [session, setSession] = useState<Session | null>(null);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const { data: authData } = supabase.auth.onAuthStateChange((_, session) => {
+      setSession(session);
+    })
+
+    return () => authData.subscription.unsubscribe();
+  })
+}
+
+export default useAuth;
