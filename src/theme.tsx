f894312e6863cfcfda8d4dd982b309853ee9619c
eiko0705 <eiko.imai.070502@gmail.com>
2024-08-14 21:32:55 +0900

    feat: create theme color

---
+++ src/theme.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme ({
+  palette: {
+    primary: {
+      main: '#ec407a',
+      light: '#ef6694',
+      dark: '#ec407a'
+    }
+  }
+});
+
+export default theme;
