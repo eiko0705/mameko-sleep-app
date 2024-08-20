89d30aaa5d8cc6bb67ab423b47c431ada31c2856
eiko0705 <eiko.imai.070502@gmail.com>
2024-08-20 14:39:15 +0900

    feat: change ui between signin and signout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,14 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 
 const Navbar = () => {
+  const [auth, setAuth] = useState<boolean>(false);
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const [accountMenuOpen, setAccountMenuOpen] = useState<boolean>(false);
   const menuAnchorEl = useRef<HTMLButtonElement>(null);
@@ -43,46 +45,12 @@ const Navbar = () => {
             aria-label="menu"
             sx={{ mr: 2 }}
             onClick={handleMenuClick}
-          >
+            >
             <MenuIcon />
           </IconButton>
-          <Menu
-            id='menu-appbar'
-            anchorEl={menuAnchorEl.current}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            open={menuOpen}
-            onClose={handleMenuClose}
-            elevation={5}
-          >
-            <MenuItem>Logs</MenuItem>
-            <MenuItem>Summary</MenuItem>
-          </Menu>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            mameko-sleep-app
-          </Typography>
-          <div>
-            <IconButton
-              ref={accountAnchorEl}
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              color="inherit"
-              onClick={handleAccountMenuClick}
-            >
-              <AccountCircle />
-            </IconButton>
             <Menu
-              id='accoutn-menu-appbar'
-              anchorEl={accountAnchorEl.current}
+              id='menu-appbar'
+              anchorEl={menuAnchorEl.current}
               anchorOrigin={{
                 vertical: 'top',
                 horizontal: 'right',
@@ -92,14 +60,70 @@ const Navbar = () => {
                 horizontal: 'right',
               }}
               keepMounted
-              open={accountMenuOpen}
-              onClose={handleAccountMenuClose}
+              open={menuOpen}
+              onClose={handleMenuClose}
               elevation={5}
-            >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Sign out</MenuItem>
+              >
+              {auth ? (
+                <>
+                  <MenuItem>Logs</MenuItem>
+                  <MenuItem>Summary</MenuItem>
+                </>
+              ) : (
+                <>
+                  <MenuItem>Log in</MenuItem>
+                  <MenuItem>Sign in</MenuItem>
+                </>
+              )}
             </Menu>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            mameko-sleep-app
+          </Typography>
+          <div>
+            {auth ? (
+              <>
+                <IconButton
+                  ref={accountAnchorEl}
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  color="inherit"
+                  onClick={handleAccountMenuClick}
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id='accoutn-menu-appbar'
+                  anchorEl={accountAnchorEl.current}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  open={accountMenuOpen}
+                  onClose={handleAccountMenuClose}
+                  elevation={5}
+                >
+                  <MenuItem>Profile</MenuItem>
+                  <MenuItem>Settings</MenuItem>
+                  <MenuItem>Sign out</MenuItem>
+                </Menu>
+              </>
+            ) : (
+              <>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  sx={{ textTransform: "none" }}
+                >Log in</Button>
+              </>
+            )}
           </div>
         </Toolbar>
       </AppBar>
