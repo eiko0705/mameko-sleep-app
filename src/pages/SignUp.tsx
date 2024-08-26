a8ef7810d4069c3a84fbd71262f0d25d2ca332a4
eiko0705 <eiko.imai.070502@gmail.com>
2024-08-26 22:19:30 +0900

    feat: import react hook form

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -8,12 +9,28 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Dayjs } from 'dayjs';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 
+type Inputs = {
+  babyName: string | null;
+  email: string | null;
+  password: string | null;
+  birthday: Date | null;
+}
+
 const SignUp = () => {
   const [value, setValue] = useState<Dayjs | null>(null)
+
+  const {
+    register,
+    control,
+    handleSubmit,
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
+    console.log(`submit: ${data}`);
+  } 
 
   return (
     <Container component='main' maxWidth='sm'>
@@ -28,7 +45,12 @@ const SignUp = () => {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <Box component='form' noValidate sx={{ mt: 3 }}>
+        <Box
+          component='form'
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          sx={{ mt: 3 }}
+        >
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
@@ -65,6 +87,7 @@ const SignUp = () => {
               <Grid item xs={12}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                   <DatePicker
+                    label='birthday'
                     slotProps={{textField: { required: true}}}
                     format='MM-DD-YYYY'
                     value={value}
@@ -84,7 +107,7 @@ const SignUp = () => {
             <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="#" variant="body2">
-                  Already have an account? Sign in
+                  Already have an account? Log in
                 </Link>
               </Grid>
             </Grid>
